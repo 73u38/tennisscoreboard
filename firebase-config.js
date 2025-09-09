9af00eb70da1f90f11cf3ea9ea6e9189c58e78b6
sakura chouyu <mongky23@gmail.com>
2025-09-09 11:10:18 +0700

    Update firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -2,9 +2,12 @@
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyAQFPP3U1W7FcQ7H40k2JNIzEjCueQAVgM",
   authDomain: "tennis-scoreboard-5d34d.firebaseapp.com",
+  databaseURL: "https://tennis-scoreboard-5d34d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "tennis-scoreboard-5d34d",
-  storageBucket: "tennis-scoreboard-5d34d.firebasestorage.app",
+  storageBucket: "tennis-scoreboard-5d34d.appspot.com",
   messagingSenderId: "714609645231",
   appId: "1:714609645231:web:aa75c47389d165ff09265a",
   measurementId: "G-F3CSE7EQP0"
+};
+
 };
